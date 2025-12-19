b7ba2f0b3096a80d2dbaaf4d4cdb4d74ada07efb
SergioReq94 <1851937@alu.murciaeduca.es>
2025-12-19 16:25:31 +0100

    6.19

---
+++ T6/Actividades de aplicación/6.19/6.19.js
@@ -0,0 +1,7 @@
+let cuenta = document.getElementsByTagName("span");
+let cuenta1 = document.getElementsByTagName("p");
+let cuenta2 = document.getElementsByTagName("a");
+
+console.log("Elementos tipo span: " + cuenta.length);
+console.log("Elementos tipo p: " + cuenta1.length);
+console.log("Elementos tipo a: " + cuenta2.length);
