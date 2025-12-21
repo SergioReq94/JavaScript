20ccfad8e8d50958855f5d43b2d2c68e2e16ca85
SergioReq <1851937@alu.murciaeduca.es>
2025-12-21 13:00:46 +0100

    Parte 1

--- T6/Actividades de aplicación/6.18/6.18.js
+++ T6/Actividades de aplicación/6.18/6.18.js
@@ -1,11 +1,16 @@
-function addFila(){
+function addFila() {
     const tabla = document.getElementById("tabla");
     const nuevaFila = document.createElement("tr");
-    const celda = document.createElement("td");
-    const texto = document.createTextNode("5. Adri");
 
-    celda.appendChild(texto);
-    nuevaFila.appendChild(celda);
+    const nombres = ["Adri", "Juan", "Pepe", "Ana"];
+
+    nombres.forEach(nombre => {
+        const celda = document.createElement("td");
+        const texto = document.createTextNode(nombre);
+        celda.appendChild(texto);
+        nuevaFila.appendChild(celda);
+    });
+
     tabla.appendChild(nuevaFila);
 }
 
